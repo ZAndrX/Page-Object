e199e0ef548c3b47daa0f7de963b7685f3792c82
ZAndrX <pyanykh.andryushka@mail.ru>
2022-09-05 18:47:26 +0500

    add LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url=="http://selenium1py.pythonanywhere.com/ru/accounts/login/", "url isn't right"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is missing on the page"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register form is missing on the page"
