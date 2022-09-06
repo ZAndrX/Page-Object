41ca458be745ae90c07b5c720f47087f5590961b
ZAndrX <pyanykh.andryushka@mail.ru>
2022-09-06 13:43:06 +0500

    add true name func

--- practicka/1_add_basket/test_product_page.py
+++ practicka/1_add_basket/test_product_page.py
@@ -3,7 +3,7 @@ import time
 from page.product_page import MainPage
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = MainPage(browser, link)
     page.open()
