6038b8e38c0622eaaff4d521298c041b83177ed6
ZAndrX <pyanykh.andryushka@mail.ru>
2022-09-06 17:54:53 +0500

    add marks

--- practicka/1_add_basket/test_product_page.py
+++ practicka/1_add_basket/test_product_page.py
@@ -1,10 +1,9 @@
-import time
-
 import pytest
 
 from page.product_page import MainPage
 
 
+@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -12,7 +11,9 @@ from page.product_page import MainPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  pytest.param(
+                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                      marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
@@ -23,21 +24,26 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_msg_add()
 
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser,
+                                                                       link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
     page = MainPage(browser, link)
     page.open()
     page.add_in_basket()
     page.should_not_be_success_message()
 
-def test_guest_cant_see_success_message(browser, link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
+
+def test_guest_cant_see_success_message(browser,
+                                        link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
     page = MainPage(browser, link)
     page.open()
     page.should_not_be_success_message()
 
 
-def test_message_disappeared_after_adding_product_to_basket(browser, link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser,
+                                                            link="http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"):
     page = MainPage(browser, link)
     page.open()
     page.add_in_basket()
     page.should_is_disappeared_msg()
-
